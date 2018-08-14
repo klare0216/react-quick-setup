69bac7d0823cc461e7ed66d62697cce0a1916d73
klareH <klare0216@gmail.com>
2018-08-14 21:47:55 +0800

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+# Introduce
+Quick react development module installation.
+
+# Installation
++ clone the file
++ type `sh install.sh` on your command line
+
+# Test
++ `npm start`
+
+
+
---
+++ install.sh
@@ -0,0 +1,3 @@
+npm i -D webpack webpack-cli webpack-dev-server babel-core babel-loader babel-plugin-transform-object-rest-spread babel-preset-env babel-preset-react css-loader html-webpack-plugin node-sass react-router-dom sass-loader style-loader url-loader 
+npm i -S react react-dom react-router
+
